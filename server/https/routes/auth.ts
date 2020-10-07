b593ddff08212c8d504e70bdcf29a51c99f7fddd
Bendimester23 <58264102+Bendimester23@users.noreply.github.com>
2020-10-07 19:25:02 +0200

    ghjngfj

--- server/https/routes/auth.ts
+++ server/https/routes/auth.ts
@@ -2,8 +2,9 @@ import Router from 'express';
 const router = Router();
 import User from '../../database/models/User'
 import Joi from '@hapi/joi';
+import crypt from 'bcryptjs';
 
-const regSchema = {
+const regSchema = Joi.object( {
     username: Joi.string()
         .min(6)
         .max(255)
@@ -17,29 +18,50 @@ const regSchema = {
         .min(6)
         .max(1024)
         .required()
-};
+});
 
 router.get('/', (req,res) => {
     res.status(200).send('auth working.');
 });
 
 router.get('/register', async (req,res) => {
-    Joi.attempt(req.body, regSchema)
+    regSchema.validate(req.body)
     .catch(err => {
-        res.status(400).send(err);
-        console.log(err);
+        res.status(400).send(err.toString());
         return;
     });
     let user = new User({
         name: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: crypt.hashSync(req.body.password,1)
     })
+
+    const nameExist = await User.exists({name: req.body.username});
+    if (nameExist) {
+        try {
+            res.json({
+                status: "error",
+                error: "Already registered!"
+            });
+        } catch (error) {
+        }
+        return;
+    }
+
     try {
         const saved = await user.save();
-        res.status(200).send(saved);
+        try {
+            res.status(200).send(saved);
+        } catch (error) {
+        }
+        return;
     }catch (err) {
-        res.status(400).send(err);
+        console.error(err);
+        try {
+            res.send("Err: IDK");
+        } catch (error) {
+        }
+        return;
     }
 });
 
